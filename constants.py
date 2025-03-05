660609076f52c6975291d5475a1ddfb2ded8d719
Atri Sarker <atri.sarker@stu.ocsb.ca>
2025-03-05 03:39:58 +0000

    Added constants.py and pizza_cost.py

---
+++ constants.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# Created By: Atri Sarker
+# Date: March 4, 2025
+# This module contains constants
+
+# Rate for Sales Tax
+HST = 0.13
+# Labour cost is $2.00 / pizza, regardless of size
+LABOUR_COST = 2.00
+# Rental cost for the shop works out to $2.25 / pizza, regardless of size
+RENTAL_COST = 2.25
+# Ingredients cost $1.50 / diameter inch of pizza
+INGREDIENT_COST_PER_INCH = 1.50
---
+++ pizza_cost.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# Created By: Atri Sarker
+# Date: March 4, 2025
+# This program asks the user for the size of a pizza,
+# which is provided through the value of the diameter in inches.
+# Using the size, the program calculates and displays the cost of the pizza
+import constants
+
+
+def main():
+    # Get the Pizza's diameter in inches
+    diameter = int(input("Enter the diameter (inches): "))
+
+    # Calculate the subtotal
+    subtotal = constants.LABOUR_COST + constants.RENTAL_COST
+    subtotal += constants.INGREDIENT_COST_PER_INCH * diameter
+    # Calculate the tax
+    tax = subtotal * constants.HST
+    # Calculate the total
+    total = subtotal + tax
+
+    # Display the total cost of the pizza to the user
+    print(f"The total cost is ${total:,.2f}")
+
+
+if __name__ == "__main__":
+    main()
